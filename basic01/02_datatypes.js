33e276dd0b98a67defbad95894a2f3cad0b84a70
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-16 14:05:52 +0000

    datatypes

---
+++ basic01/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js code as newer version
+
+// alert("hello") // we are using node js, not browser
+
+console.log(3+3);
+
+let name = "Amna"
+let age = 18
+let isLoggedIn = false
+let state;
+let temp = null
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true /false
+// null  => standalone value // output object type
+// undefined => undefine
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "amna");
