5283423fed5412f25e1653ccb90348fb70206349
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-16 13:26:43 +0000

    container code done

---
+++ basic01/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "amna@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rwp"
+
+// accountId = 2
+accountEmail = "hc@gmail.com"
+accountPassword = "22444"
+accountCity = "islbd"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var
+beacuse of block scope and functional scope
+*/
