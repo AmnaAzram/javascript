96e74aaca21580fbb8a41e1c26c0461c86593f96
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-17 18:17:19 +0000

    datatype_summary

---
+++ basic01/datatype_summary.js
@@ -0,0 +1,17 @@
+// There are two type of data types
+// Primitive ==> 7 types of primitive data type ==> give a copy
+// string, number , boolean, null , undefine , bigint, symbol
+
+// NonPrimitive(Refrence) ==> change a original
+//  array , object , function
+
+let str = "Amna" 
+let num = 99
+let bool = true
+let symbol = Symbol("132")
+let bigint = 345787654323n
+let empty = null
+let undfn;
+
+console.log(typeof bigint);
+console.log(typeof symbol);
