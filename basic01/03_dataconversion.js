a73e3e967d4464c5dad908931722a004927d4307
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-17 17:52:01 +0000

    datatype conversion

---
+++ basic01/03_dataconversion.js
@@ -0,0 +1,23 @@
+// let score = 33
+// console.log(typeof score);
+
+let score = "33"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let tem = null
+let value = Number(tem)
+console.log(value);
+console.log(typeof value);
+
+// conversion in boolean
+
+let booleanIs = 1
+let logIn = Boolean(booleanIs)
+console.log(typeof logIn);
+console.log(logIn);
+
+let str = 33
+let string = String(str)
+console.log(string);
+console.log(typeof string);
