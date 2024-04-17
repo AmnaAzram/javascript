80f37a7eefa0615f2c1c53f01117a5ed6df846d8
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-17 18:08:25 +0000

    compersion

---
+++ basic01/05_compersion.js
@@ -0,0 +1,15 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2!=1);
+
+// null compersion
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null != 0);
+
+// 
+
