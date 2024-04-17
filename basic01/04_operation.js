221f905f18f8143d85a412518fff4f82efc3c564
AmnaAzram <141334222+AmnaAzram@users.noreply.github.com>
2024-04-17 18:02:29 +0000

    operation

---
+++ basic01/04_operation.js
@@ -0,0 +1,41 @@
+
+// change into negative
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+// operation in javascript
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+
+//  string operation
+let str1 = "Amna"
+let str2 = " Azram"
+let str3 = str1 + str2
+console.log(str3);
+// depends on first value
+console.log("2" + 2 +1);
+console.log(2+2+"1");
+
+// increment & decrement
+// prefix , postfix
+
+let game = 100
+game++
+console.log(game);
+// prefix
+let x = 6
+let y = ++x
+console.log(x);
+console.log(y);
+
+// postfix
+let a = 7
+let b = a++
+console.log(a);
+console.log(b);
